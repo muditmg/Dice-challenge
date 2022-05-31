32968fcb66ffb9e8d280ab887d78a556b64acac3
muditmg <mudit.gupta02100@gmail.com>
2022-05-31 12:58:41 +0530

    no commits yet

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber1= Math.floor(Math.random() * 6)+1;
+var randomImages= "dice"+ randomNumber1 + ".png";
+var imageSource="images/" +randomImages;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",imageSource);
+
+var randomNumber2= Math.floor(Math.random() * 6)+1;
+var randomImages2= "dice"+ randomNumber2 + ".png";
+var imageSource2="images/" +randomImages2;
+
+document.querySelectorAll("img")[1].setAttribute("src", imageSource2)
+
+
+
+if (randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🏆 Player 1 wins"
+}
+else if (randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML="🏆 Player 2 wins"
+
+}
+else{
+  document.querySelector("h1").innerHTML="DRAW!"
+
+}
